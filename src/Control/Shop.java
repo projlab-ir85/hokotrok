eccba3bc40688e6d19ccc44322a930868c35720e
Göcző Bence <goczo.bence@edu.bme.hu>
2026-03-19 15:05:21 +0100

    Add Player and Shop classes with initial stubs

---
+++ src/Control/Shop.java
@@ -0,0 +1,20 @@
+package Control;
+
+import Vehicles.Snowplow;
+
+public class Shop {
+
+
+    void buySnowplow(Player player){
+
+    }
+    void buyBus(Player player){
+
+    }
+    void buyConsumeable(Player player, Snowplow vehicle, String consumeable){
+
+    }
+    void buyAttachment(Player player,Snowplow vehicle, String plow){
+
+    }
+}
