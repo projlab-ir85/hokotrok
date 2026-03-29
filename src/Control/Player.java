2b6190347e3403de9d2d5ef93aeef6cc042472ab
Bálint Kérészy <balint@macbookpro.vpk.bme.hu>
2026-03-30 00:01:51 +0200

    Comments added to control

--- src/Control/Player.java
+++ src/Control/Player.java
@@ -3,25 +3,36 @@ import java.util.List;
 
 import Vehicles.*;
 
-
+/** A játékos megvalósítása */
 public class Player {
     private List<Vehicle> vehicles;
     private int cash;
-
+    /** Konstruktor amiben meg lehet mennyi pénzzel kezd a játékos
+     * Létrehozza a járművek listáját és beállítja a kezdő pénzmennyiséget
+     * @param startingCash mennyi pénzzel kezd a játékos
+     */
     public Player(int startingCash) {
         vehicles = new java.util.ArrayList<>();
         cash = startingCash;
     }
-
+    /** Jármű hozzáadása, hozzáad egy járművet a játékos járműinek listájához
+     * @param v a jármű amit odaadunk a játékosnak
+     */
     public void addVehicle(Vehicle v) {
         vehicles.add(v);
     }
-
+    /** Pénz csökkentés, csökkentjük a játékos pénzét ha vásárol a megadott mennyiséggel
+     * @param amount a mennyiség amivel a játékos a pénzét csökkentjük
+     */
     public void decreaseCash(int amount) {
         cash -= amount;
     }
-
+    /** Visszaadja mennyi pénze van a játékosnak
+     * @return a játékos pénzmennyisége
+     */
     public int getCash() { return cash; }
-
+    /** Visszaadja mennyi járműve van a játékosnak
+     * @return a jármű lista mérete
+     */
     public int getVehicleCount() { return vehicles.size(); }
 }
--- src/Control/Shop.java
+++ src/Control/Shop.java
@@ -3,28 +3,51 @@ package Control;
 import Attachments.PlowHead;
 import Attachments.Snowchain;
 import Vehicles.*;
-
+/**A bolt megvalósítása */
 public class Shop {
-
+    /** Default konstruktor
+     */
     public Shop() {}
-
+    /** Hókotró vásárlása
+     * Csökkenti a játékos pénzét és ad egy új hókotrót
+     * @param player melyik játékosnak adjuk a hókotrót
+     */
     public void buySnowplow(Player player){
         player.decreaseCash(2000);
         player.addVehicle(new Snowplow());
     }
+    /** Busz vásárlása
+     * Csökkenti a játékos pénzét és ad egy új buszt
+     * @param player melyik játékosnak adjuk a buszt
+     */
     public void buyBus(Player player){
         player.decreaseCash(2000);
         player.addVehicle(new Bus());
     }
-    public void buyConsumeable(Player player, Snowplow vehicle, String type){
+    /** Fogyóeszköz vásárlása
+     * Levonjuk a játékostól a pénzt és megtöltjük az adott hókotró kotrófejét
+     * @param player melyik játékostól vonjuk le a pénzt
+     * @param vehicle melyik hókotrónak adjuk a fogyóeszközt
+     */
+    public void buyConsumeable(Player player, Snowplow vehicle){
         player.decreaseCash(100);
         vehicle.fillActiveHead(10);
     }
-
+    /** Busz vásárlása
+     * Levonjuk a játékostól a pénzt és felteszzük a buszra a hóláncot
+     * @param player melyik játékos veszi a hóláncot
+     * @param bus melyik buszra vesszük a hóláncot
+     */
     public void buySnowchain(Player player, Bus bus){
         player.decreaseCash(300);
         bus.addSnowchain(new Snowchain(30));
     }
+    /** Kotrófej vásárlása
+     * Levonjuk a játékostól a pénzt, majd az adott hókotróhoz hozzáadjuk az adott kotrófejet
+     * @param player melyik játékos veszi a kotrófejet
+     * @param vehicle melyik hókotróra vesszük a kotrófejet
+     * @param plow milyen kotrófejet veszünk
+     */
     public void buyAttachment(Player player, Snowplow vehicle, PlowHead plow){
         player.decreaseCash(500);
         vehicle.addPlow(plow);
