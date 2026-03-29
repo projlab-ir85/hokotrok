ab1be85a92003b4639f5b636d8f87ca98f31e877
Bálint Kérészy <balint@macbookpro.vpk.bme.hu>
2026-03-29 22:57:50 +0200

    Plowheads comments complete

--- src/Attachments/PlowHeads/IceBreakerHead.java
+++ src/Attachments/PlowHeads/IceBreakerHead.java
@@ -3,10 +3,15 @@ package Attachments.PlowHeads;
 import Attachments.PlowHead;
 import RoadComponents.RoadSection;
 
+/**Jégtörő fej megvalósítása*/
 public class IceBreakerHead extends PlowHead {
+    /** Default konstruktor */
     public IceBreakerHead() {}
-
+    /** Kotrófej use függvény megvalósítása
+    * @param roadsection az adott útszakasz ahol a jeget takarítjuk
+    */
     public void use(RoadSection roadsection){
+        /**Megszüntetjük a jeget az útról viszont a hó mennyiségét növeljük */
         int ice = roadsection.getIce();
         roadsection.iceReduce(ice);
         roadsection.snowIncrease(ice);
--- src/Attachments/PlowHeads/SaltShakerHead.java
+++ src/Attachments/PlowHeads/SaltShakerHead.java
@@ -4,11 +4,19 @@ import Attachments.PlowHead;
 import Consumable.Consumable;
 import RoadComponents.RoadSection;
 
+/**Sószóró fej megvalósítása*/
 public class SaltShakerHead extends PlowHead {
+    /** Default konstruktor */
     public SaltShakerHead() {}
-
+    /**létrehozunk egy sót*/
     private Consumable salt = new Consumable(60, 2);
+    /** Kotrófej use függvény megvalósítása
+    * @param roadsection az adott útszakasz ahol a havat és jeget takarítjuk
+    */
     public void use(RoadSection roadsection){
+        /** Ha van még fogyóeszközünk akkor hozzáadjuk az útszakaszhoz
+         * és csökkentjük a fogyóeszközök mennyiségét
+         */
         if(consumableAmountLeft>0) {
         roadsection.addConsumable(salt);
         consumableAmountLeft--;
