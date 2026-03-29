3710dd67f666252c4dc317e855c67c8661e01829
Göcző Bence <52159134+bence0585@users.noreply.github.com>
2026-03-29 23:04:08 +0200

    Fix Snowchain: store initial timeToLive and correct fix methot to return to the intial value

--- src/Attachments/Snowchain.java
+++ src/Attachments/Snowchain.java
@@ -4,9 +4,11 @@ import RoadComponents.RoadSection;
 
 public class Snowchain extends Attachment {
     protected int timeToLive;
+    private int initialTimeToLive;
 
     public Snowchain(int timeToLive){
         this.timeToLive = timeToLive;
+        this.initialTimeToLive = timeToLive;
     }
 
     public void use(){
@@ -14,7 +16,7 @@ public class Snowchain extends Attachment {
     }
 
     public void fix(){
-        timeToLive = 30;
+        timeToLive = initialTimeToLive;
     }
 
     public int getTimeToLive(){return timeToLive;}
